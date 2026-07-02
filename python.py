76c18992952c86fe7560a169ea2cb87bb73da07f
krishit senghani <krishitsenghani22@gmail.com>
2026-07-02 17:17:10 +0530

    variables

---
+++ python.py
@@ -0,0 +1,16 @@
+print("my first python code")
+print("Hello wrold")
+print("i can learn coding")
+print(2 + 3 * 4)
+
+
+# Variables
+
+name = "Krishit"
+age = 20
+price = 99.5
+is_student = True
+print(name)
+print(age)
+print(price)
+print(is_student)
