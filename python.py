9b249a3ef77eae976be176edacd16f5923772429
krishit senghani <krishitsenghani22@gmail.com>
2026-07-05 16:43:55 +0530

    task

--- python.py
+++ python.py
@@ -2,21 +2,37 @@
 
 # TASK :-
 
-# 1. Calculate grade from marks using if-else
+# 1
+gen = input ("please tell  your gender in (M or F) :- ")
 
-marks = int(input("enter your marks :-"))
-
-if marks >= 90:
-    print("you got A+ grade")
-elif marks >= 80 :
-    print("you got A grade")
-elif marks >= 70 :
-    print("you got B+ grade")
-elif marks >= 60 :
-    print("you got B grade")
-elif marks >= 50 :
-    print("you got C grade")
-elif marks >= 40 :
-    print("you got C+ grade")
+if gen == "M" or gen =="m" :
+    print("Hello Sir")
+elif gen == "F" or gen == "f" :
+    print("Hello Mam")
 else :
-    print("you are fail")
+    print("Hello Sir/Mam")
+
+
+# 2
+
+year = int(input("Enter the year :- "))
+
+if year % 100 == 0 or  year % 4 == 0 :
+    print("leap year")
+elif year % 100 != 0 and year % 4 == 0 : 
+    print("leap year")
+else : 
+    print("Not a leap year")
+
+
+# 3
+
+tem = int(input("Enter your temperature :-"))
+ 
+if tem <= -5 or tem <= 5:
+    print("its very cold")
+elif tem <= 6 or tem <= 18:
+    print("its cold") 
+else :
+    print("its hot")
+
