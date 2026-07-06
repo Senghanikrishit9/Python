4f8b4ad5e4c5c3c1576fe435a3d32f7d429984c7
krishit senghani <krishitsenghani22@gmail.com>
2026-07-06 20:16:53 +0530

    loops

--- python.py
+++ python.py
@@ -1,38 +1,22 @@
-# 1 . If else-statment
+# 1 . Loops
 
-# TASK :-
+# 1) Print numbers from 1 to 5
 
-# 1
-gen = input ("please tell  your gender in (M or F) :- ")
+for i in range(1, 6):
+    print(i)
 
-if gen == "M" or gen =="m" :
-    print("Hello Sir")
-elif gen == "F" or gen == "f" :
-    print("Hello Mam")
-else :
-    print("Hello Sir/Mam")
+# 2) Print each item in a list
 
+fruits = ["apple", "banana", "mango"]
 
-# 2
-
-year = int(input("Enter the year :- "))
-
-if year % 100 == 0 or  year % 4 == 0 :
-    print("leap year")
-elif year % 100 != 0 and year % 4 == 0 : 
-    print("leap year")
-else : 
-    print("Not a leap year")
+for fruit in fruits:
+    print(fruit)
 
 
-# 3
+# 3) Print 1 to 5 using while loop
 
-tem = int(input("Enter your temperature :-"))
- 
-if tem <= -5 or tem <= 5:
-    print("its very cold")
-elif tem <= 6 or tem <= 18:
-    print("its cold") 
-else :
-    print("its hot")
+i = 1
 
+while i <= 5:
+    print(i)
+    i += 1
