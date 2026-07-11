5e50a5dd7703cc50cdee03d1bfe001321707cb2d
krishit senghani <krishitsenghani22@gmail.com>
2026-07-11 10:12:20 +0530

    loops tasks

--- python.py
+++ python.py
@@ -1,40 +1,26 @@
-# Task
-
-n = int(input("Enter the number :-"))
-
-s = 0
-for i in range(1,n):
-    if n % i == 0:
-        s = s + i
-
-if s == n:
-    print("perfect number")
-else:
-    print("looser")
-
-
-
-n = int(input("Enter the number :-"))
-count = 0
-
-for i in range(n,n+1):
-    if  n % i == 2:
-        count = count + 1
-if count == 2:
-    print("prime number")
-else:
-    print("not a prime number")
-
-
-s = input("Enter the name :-")
+s = input("Enter the name :- ")
 rev = ""
-for i in range(len(s)-1,-1,-1):
+for i in range (len(s)-1,-1,-1):
     rev = rev + s[i]
 
-if rev == s:
-    print("palidrome")
+if rev == s :
+    print("palidrome ")
 else :
-    print("Not palidrome")
-    
+    print("Not a palidrome")
 
-       
+
+a = "PYRrjahfff%^&@#dkjpvhg123456"
+
+char = 0
+spical = 0
+apl = 0
+
+
+for i in a:
+    if(ord(i) >= 48 or ord(i) <= 57):
+        print(f"char :- {char}")
+    elif(ord(i) >= 65 or ord(i) <= 90) or (ord(i) >=97 and ord(i) <= 122):
+        print(f"apl :- {apl}")
+    else :
+        print(f"spical :- {spical}")
+
