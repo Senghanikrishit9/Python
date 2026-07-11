b43333e4d68f23af13c1a64e63e746af3591db38
krishit senghani <krishitsenghani22@gmail.com>
2026-07-11 10:34:33 +0530

    while loops

--- python.py
+++ python.py
@@ -1,26 +1,15 @@
-s = input("Enter the name :- ")
-rev = ""
-for i in range (len(s)-1,-1,-1):
-    rev = rev + s[i]
+# 1 . While loops
 
-if rev == s :
-    print("palidrome ")
-else :
-    print("Not a palidrome")
+a = 1  
+while a != 20:
+    print(a)
+    a = a + 1
 
 
-a = "PYRrjahfff%^&@#dkjpvhg123456"
 
-char = 0
-spical = 0
-apl = 0
+count = 0
+while count <= 70:
+    print(count)
+    count = count + 1
 
-
-for i in a:
-    if(ord(i) >= 48 or ord(i) <= 57):
-        print(f"char :- {char}")
-    elif(ord(i) >= 65 or ord(i) <= 90) or (ord(i) >=97 and ord(i) <= 122):
-        print(f"apl :- {apl}")
-    else :
-        print(f"spical :- {spical}")
 
